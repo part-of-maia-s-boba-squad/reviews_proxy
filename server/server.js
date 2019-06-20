cde292bb6bfa8c5f0f3e95e21466c08e2b857073
Al Bungay <alerterb@gmail.com>
2019-06-19 17:26:48 -0700

    Have proxy exress endpoints now pointing to ec2 instance

--- server/server.js
+++ server/server.js
@@ -1,48 +1,47 @@
-const express = require('express');
+const relic = require('newrelic');
 const bodyParser = require('body-parser');
+const express = require('express');
 const path = require('path');
-const proxy = require('http-proxy-middleware');
-const PORT = 3000;
+proxy = require('http-proxy-middleware');
+const cors = require ('cors');
+
 const app = express();
+// !!!!!!!!! DONT PARSE WHEN SENDING FROM PROXY FOR POST
+// app.use(bodyParser.json());
+app.use(cors());
+const port = 3010;
+const publicDir = path.join(__dirname, '../public/');
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(publicDir));
+app.use('/review/', proxy('http://18.221.71.152/review/'));
+app.use('/addReview', proxy('http://18.221.71.152/addReview'));
 
-var resOptions = {
-    target: 'http://localhost:3010/', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-}
+app.get('/:id', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../public') + '/index.html');
+});
+
+app.listen(port, () => console.log('Listening on Port:', port));
+
+// var resOptions = {
+//     target: 'http://localhost:3010/', // target host
+//     changeOrigin: true, // needed for virtual hosted sites
+//     ws: true, // proxy websockets
+// }
    
-app.use('/reservations', proxy(resOptions));
+// app.use('/reservations', proxy(resOptions));
 
-var menuOptions = {
-    target: 'http://localhost:3003/', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-}
+// var menuOptions = {
+//     target: 'http://localhost:3003/', // target host
+//     changeOrigin: true, // needed for virtual hosted sites
+//     ws: true, // proxy websockets
+// }
 
-app.use('/API/restaurant', proxy(menuOptions));
+// app.use('/API/restaurant', proxy(menuOptions));
 
-var picOptions = {
-    target: 'http://localhost:3002/', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-}
+// var picOptions = {
+//     target: 'http://localhost:3002/', // target host
+//     changeOrigin: true, // needed for virtual hosted sites
+//     ws: true, // proxy websockets
+// }
 
-app.use('/restaurants', proxy(picOptions));
-
-var reviewOptions = {
-    target: 'http://localhost:3007/', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
-}
-
-app.use('/reviews', proxy(reviewOptions));
-
-app.get('/restaurant/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log('Listening on port', PORT);
-});
+// app.use('/restaurants', proxy(picOptions));
